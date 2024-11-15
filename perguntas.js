72eaaba322e23e1a7ce08019d0ee5b5a3dce3b48
Lana Prestes <138732318+xuxuvonmell@users.noreply.github.com>
2024-11-15 12:27:24 -0300

    cartões.

---
+++ perguntas.js
@@ -0,0 +1,143 @@
+criaCartao (
+    'Programação',
+    'Para que serve o main de uma pagina html?',
+    'Serve para eu anexar todo conteúdo principal do meu site'
+)
+
+criaCartao (
+    'Robótica',
+    'Por que o carrinho de robótica não deu certo?',
+    'Estava com mal contato no jumper que conectava na placa'
+)
+
+criaCartao (
+    'História',
+    'Quais foram algumas das principais revoltas que ocorreram durante a Primeira República do Brasil?',
+    'Revolta das vacinas, revolta de canudos, revolução federalista, movimento do contestado.'
+)
+
+criaCartao (
+    'Geografia',
+    'Quais são as estações do ano ?',
+    'Outono, Inverno, Verão, Primavera'
+)
+
+criaCartao (
+    'Geografia',
+    'O que é neoliberalismo?',
+    'É uma corrente econômica que defende a liberdade de mercado, a desregulamentação e a redução do papel do Estado na economia.'
+)
+
+criaCartao (
+    'Geografia',
+    'O que é o desemprego estrutural?',
+    'Refere-se a mudanças de longo prazo na economia que afetam a demanda para certos tipos de trabalho.'
+)
+
+criaCartao (
+    'Geografia',
+    'O que é o desemprego conjuntural?',
+    'Resulta de flutuações de curto prazo no ciclo econômico.'
+)
+
+criaCartao (
+    'Geografia',
+    'Quais são as responsabilidades da ONU?',
+    'Manter a paz e a segurança internacional, desenvolver relações amistosas entre as nações,entre outros.'
+)
+
+criaCartao (
+    'Geografia',
+    'Quais os países que formam o BRICS?',
+    'Brasil, Rússia, Índia, China e África do Sul.'
+)
+
+criaCartao (
+    'Geografia',
+    'O que é um produto manufaturado?',
+    'São aqueles que são fabricados em grande quantidade, de forma padronizada e em série.'
+)
+
+criaCartao (
+    'Geografia',
+    'Qual país que mais importa produtos brasileiros?',
+    'O chile.'
+)
+
+criaCartao (
+    'Geografia',
+    'O que é gentrificação?',
+    'É processo que ocorre em áreas urbanas quando em uma área que antes era considerada degradada ou carente começa a atrair investimentos e novos moradores com poder aquisitivo maior.'
+)
+
+criaCartao (
+    'Geografia',
+    'O que é especulação imobiliária?',
+    'É a prática de pessoas de comprar imóveis esperando que seus preços aumentem no futuro para vendê-los com lucro.'
+)
+
+criaCartao (
+    'Geografia',
+    'Quais os problemas enfrentados nas fronteiras brasileiras?',
+    'Tráfico de drogas, contrabando, imigração ilegal.'
+)
+
+criaCartao (
+    'Geografia',
+    'Quem faz o controle das fronteiras brasileiras?',
+    'A Polícia Federal, Receita Federal, Forças Armadas e agentes da Vigilância Sanitária.'
+)
+
+criaCartao (
+    'Geografia',
+    'O que é contrabando?',
+    'São importações e exportações de produtos proibidos no país.'
+)
+
+criaCartao (
+    'Geografia',
+    'O que é descaminho?',
+    'São produtos comercializados sem o recolhimento do imposto.'
+)
+
+criaCartao (
+    'Geografia',
+    'Qual é o órgão no Brasil responsável pela demarcação de terras índigenas no Brasil?',
+    'A FUNAI (Fundação Nacional dos Povos Indígenas).'
+)
+
+criaCartao (
+    'História',
+    'Quais eram as principais características do governo da Primeira República no Brasil?',
+    'Características: Governo dominado pela elite agrária, com poder concentrado em São Paulo e Minas Gerais e baixa participação popular'
+)
+
+criaCartao (
+    'História',
+    'Qual era o papel dos "coronéis" na política durante a Primeira República?',
+    'Coronéis: Líderes locais que controlavam votos e exerciam forte influência política nas regiões rurais'
+)
+
+criaCartao (
+    'História',
+    'O que motivou a Revolta da Armada?',
+    'Revolta da Armada: Foi motivada pela insatisfação da Marinha com o governo e pela falta de condições de trabalho'
+)
+
+criaCartao (
+    'História',
+    'Qual era o objetivo do movimento conhecido como Canudos?',
+    'Canudos: Liderado por Antônio Conselheiro, o movimento buscava criar uma comunidade autossuficiente e religiosa, rejeitando o governo da República.'
+)
+
+criaCartao (
+    'História',
+    'Como o Cangaço refletia as condições sociais do Brasil na época?',
+    'Cangaço: Refletia a violência e a injustiça social no sertão nordestino, com bandos armados desafiando o poder local.'
+)
+
+criaCartao (
+    'História',
+    'Qual foi o impacto da Revolta do Contestado para os moradores locais?',
+    'Revolta do Contestado: Causou conflitos entre moradores locais e autoridades por causa de terras desapropriadas para uma estrada de ferro.'
+)
